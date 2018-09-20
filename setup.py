e7bdaffcf7ef44b1820818338ef53e0dbf668802
Sharath C <sharath.c@zerodha.com>
2018-09-20 15:45:27 +0530

    setup.py updated to handle pip.req

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 import re, ast
 
 # get version from __version__ variable in frappe_s3_attachment/__init__.py
