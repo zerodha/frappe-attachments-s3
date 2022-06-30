26d8216470b803b93dba39df065593f2b9073b76
pythonpen <43410945+pythonpen@users.noreply.github.com>
2022-06-30 10:18:23 +0530

    adding end_point url for s3 client
    adding endpoing url for s3 client

--- frappe_s3_attachment/controller.py
+++ frappe_s3_attachment/controller.py
@@ -34,6 +34,7 @@ class S3Operations(object):
                 aws_access_key_id=self.s3_settings_doc.aws_key,
                 aws_secret_access_key=self.s3_settings_doc.aws_secret,
                 region_name=self.s3_settings_doc.region_name,
+                endpoint_url="https://s3." + self.s3_settings_doc.region_name + ".amazonaws.com",
             )
         else:
             self.S3_CLIENT = boto3.client('s3')
