ff18173c62b26ad91c4e05f3da39c9a1613f6527
krupalvora-SSE <krupal.v@solarsquare.in>
2026-03-03 15:40:41 +0000

    fix: s3 private files

--- frappe_s3_attachment/controller.py
+++ frappe_s3_attachment/controller.py
@@ -127,10 +127,8 @@ class S3Operations(object):
                     file_path, self.BUCKET, key,
                     ExtraArgs={
                         "ContentType": content_type,
-                        "ACL": 'public-read',
                         "Metadata": {
                             "ContentType": content_type,
-
                         }
                     }
                 )
@@ -206,15 +204,8 @@ def file_upload_to_s3(doc, method):
             parent_name
         )
 
-        if doc.is_private:
-            method = "frappe_s3_attachment.controller.generate_file"
-            file_url = """/api/method/{0}?key={1}&file_name={2}""".format(method, key, doc.file_name)
-        else:
-            file_url = '{}/{}/{}'.format(
-                s3_upload.S3_CLIENT.meta.endpoint_url,
-                s3_upload.BUCKET,
-                key
-            )
+        generate_method = "frappe_s3_attachment.controller.generate_file"
+        file_url = """/api/method/{0}?key={1}&file_name={2}""".format(generate_method, key, doc.file_name)
         os.remove(file_path)
         frappe.db.sql("""UPDATE `tabFile` SET file_url=%s, folder=%s,
             old_parent=%s, content_hash=%s WHERE name=%s""", (
@@ -270,15 +261,8 @@ def upload_existing_files_s3(name):
             parent_name
         )
 
-        if doc.is_private:
-            method = "frappe_s3_attachment.controller.generate_file"
-            file_url = """/api/method/{0}?key={1}""".format(method, key)
-        else:
-            file_url = '{}/{}/{}'.format(
-                s3_upload.S3_CLIENT.meta.endpoint_url,
-                s3_upload.BUCKET,
-                key
-            )
+        generate_method = "frappe_s3_attachment.controller.generate_file"
+        file_url = """/api/method/{0}?key={1}&file_name={2}""".format(generate_method, key, doc.file_name)
 
         # Remove file from local.
         os.remove(file_path)
