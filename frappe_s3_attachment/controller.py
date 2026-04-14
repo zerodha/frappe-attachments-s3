a35ab8dbdcf38447aac469b4181922a4f1d2c7b3
krupalvora-SSE <krupal.v@solarsquare.in>
2026-04-14 13:29:30 +0000

    fix: data import files

--- frappe_s3_attachment/controller.py
+++ frappe_s3_attachment/controller.py
@@ -194,7 +194,7 @@ def file_upload_to_s3(doc, method):
     site_path = frappe.utils.get_site_path()
     parent_doctype = doc.attached_to_doctype or 'File'
     parent_name = doc.attached_to_name
-    ignore_s3_upload_for_doctype = frappe.local.conf.get('ignore_s3_upload_for_doctype') or ['Data Import']
+    ignore_s3_upload_for_doctype = frappe.local.conf.get('ignore_s3_upload_for_doctype', [])
     if parent_doctype not in ignore_s3_upload_for_doctype:
         if not doc.is_private:
             file_path = site_path + '/public' + path
